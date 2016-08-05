7c9cbfa4fad4f5f8ee53a109f8ad65d43d3a9003
Sam <shmueld770@hotmail.com>
2016-08-05 13:40:11 -0400

    Added Table View and Instructions. Need to Fix Constraints

---
+++ Moshiach770Brachahs/menuTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  menuTableViewCell.swift
+//  Moshiach770Brachahs
+//
+//  Created by Shmuel Dlinn on 8/4/16.
+//  Copyright © 2016 Shmuel Dlinn. All rights reserved.
+//
+
+import UIKit
+
+class menuTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var menuItem: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
