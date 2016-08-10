b9cfaf6df2a90333f1410de7f95c6794eda36c71
Sam <shmueld770@hotmail.com>
2016-08-10 12:54:11 -0400

    Scroll view is working with the labels

---
+++ Moshiach770Brachahs/InformationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  InformationViewController.swift
+//  Moshiach770Brachahs
+//
+//  Created by Shmuel Dlinn on 8/8/16.
+//  Copyright © 2016 Shmuel Dlinn. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    @IBOutlet weak var InformationLabel: UILabel!
+    @IBOutlet weak var SampleLetter: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.contentSize = CGSizeMake(400, 3000)
+        
+        let text = "    When writing to the Rebbe King Moshiach Shlita include your Jewish Name and Mothers’s name (a non-Jew writes their father’s name) and likewise for all those you are requesting a blessing for (family name is not necessary). Good news is also something to write to the Rebbe King Moshiach Shlita and god resolutions to add in Torah and Mitzvos (especially Torah study about Moshiach and Tzedakah donations) which will add in receiving the blessing. Mainly accept the kingship of Moshiach by writing “Long Live the Rebbe King Moshiach forever) to his majesty. The best is to visit the Rebbe King Moshiach Shlita in person in 770 Eastern pkwy Brooklyn, New York at Sunday dollars etc. if this is not possible this app with get the letter to Moshiach."
+        InformationLabel.text = text
+        InformationLabel.requiredHeight();
+        
+        let text2 = "Sample Letter:\n\n26th of Iyar 5776\n\nRebbe King Moshiach Shlita!\nWe are happy to notify your majesty that we have put up 25 Mezuzahs today on three different homes. One of the people Avraham ben Sarah, asked for a Berachah. We hope in this merit we will see the building of the Beis Hamikdosh now! Long live the Rebbe King Moshiach Forever!\nSincerely,\nNasan Ben Serach"
+        SampleLetter.text = text2
+        SampleLetter.requiredHeight()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Moshiach770Brachahs/InstructionViewController.swift
@@ -0,0 +1,59 @@
+//
+//  InstructionViewController.swift
+//  Moshiach770Brachahs
+//
+//  Created by Shmuel Dlinn on 8/9/16.
+//  Copyright © 2016 Shmuel Dlinn. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel{
+    func requiredHeight() -> CGFloat {
+        let label:UILabel = UILabel(frame: CGRectMake(0, 0, self.frame.width, CGFloat.max))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        label.font = self.font
+        label.text = self.text
+        
+        label.sizeToFit()
+        
+        return label.frame.height;
+    }
+}
+
+
+class InstructionViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSizeMake(4000, 3000)
+        
+        let text = "    An example of a Miracle through a blessing of the Rebbe King Moshiach Shlita: In 5770 (2010), A. severely injured his leg in an accident. He had an operation which was supposed to improve his condition, but it was unsuccessful. It was hard for him to walk, and he had constant, intense pain. His friend, a Chabad rabbi, offered to write to the Rebbe King Moshiach Shlita, requesting a blessing for his complete recovery. By Divine providence he came across a letter of the Rebbe King Moshiach Shlita which says that a Jew should never feel down, and ends by blessing him with numerous blessings. A few months later, the rabbi told A. that he was traveling to Brooklyn to see the Rebbe King Moshiach Shlita and could request a blessing from the Rebbe King Moshiach Shlita in person at dollars. A. told him to tell the Rebbe King Moshiach Shlita that if his leg would be healed, he would become a completely observant Jew. When the rabbi went to receive a dollar and blessing from the Rebbe King Moshiach Shlita, he requested a blessing for A. and added that A. had said that he would become completely observant if he would recover.\n\n\tThe rabbi returned from his trip. A while later A. told the rabbi that his situation was not improving. Later that day, the rabbi came across a holy letter of the Rebbe King Moshiach Shlita. It spoke about the fact that a Jew’s physical success is intricately connected with his performance of the Commandments. The letter goes on to say that just like it isn’t enough to believe that it is important to eat – one must actually eat in order to gain strength, so too it is not enough to believe in G–d and His Commandments – a Jew must actually perform the Commandments, and only then will things go well for him. At the end of the letter, the Rebbe King Moshiach Shlita guarantees that his physical matters will be successful if he will keep the Commandments, and is awaiting good news. As a result of this letter, A. began to strengthen his performance of the Commandments, including praying, putting on Tefillin and studying Torah regularly.\n\n\tA short while later, A.’s doctors decided to try the previously unsuccessful operation again. Because of A.’s efforts in performing the Commandments, this time the operation was completely successful! A. now walks normally and you cannot even tell that he ever had an accident!\n\nFor more information see www.LivingMoshiach.com"
+        
+        textLabel.text = text
+        textLabel.requiredHeight();
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
