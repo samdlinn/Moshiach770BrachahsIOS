b7020f8324b9ec4d53a6bc3cd1db1624f0b18a69
Sam <shmueld770@hotmail.com>
2016-08-10 14:06:09 -0400

    Changed Labels to TextView for Information and Instruction

--- Moshiach770Brachahs/InformationViewController.swift
+++ Moshiach770Brachahs/InformationViewController.swift
@@ -10,22 +10,15 @@ import UIKit
 
 class InformationViewController: UIViewController {
 
-    @IBOutlet weak var InformationLabel: UILabel!
-    @IBOutlet weak var SampleLetter: UILabel!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView.contentSize = CGSizeMake(400, 3000)
-        
-        let text = "    When writing to the Rebbe King Moshiach Shlita include your Jewish Name and Mothers’s name (a non-Jew writes their father’s name) and likewise for all those you are requesting a blessing for (family name is not necessary). Good news is also something to write to the Rebbe King Moshiach Shlita and god resolutions to add in Torah and Mitzvos (especially Torah study about Moshiach and Tzedakah donations) which will add in receiving the blessing. Mainly accept the kingship of Moshiach by writing “Long Live the Rebbe King Moshiach forever) to his majesty. The best is to visit the Rebbe King Moshiach Shlita in person in 770 Eastern pkwy Brooklyn, New York at Sunday dollars etc. if this is not possible this app with get the letter to Moshiach."
-        InformationLabel.text = text
-        InformationLabel.requiredHeight();
-        
-        let text2 = "Sample Letter:\n\n26th of Iyar 5776\n\nRebbe King Moshiach Shlita!\nWe are happy to notify your majesty that we have put up 25 Mezuzahs today on three different homes. One of the people Avraham ben Sarah, asked for a Berachah. We hope in this merit we will see the building of the Beis Hamikdosh now! Long live the Rebbe King Moshiach Forever!\nSincerely,\nNasan Ben Serach"
-        SampleLetter.text = text2
-        SampleLetter.requiredHeight()
+        let text = "    When writing to the Rebbe King Moshiach Shlita include your Jewish Name and Mothers’s name (a non-Jew writes their father’s name) and likewise for all those you are requesting a blessing for (family name is not necessary). Good news is also something to write to the Rebbe King Moshiach Shlita and god resolutions to add in Torah and Mitzvos (especially Torah study about Moshiach and Tzedakah donations) which will add in receiving the blessing. Mainly accept the kingship of Moshiach by writing “Long Live the Rebbe King Moshiach forever) to his majesty. The best is to visit the Rebbe King Moshiach Shlita in person in 770 Eastern pkwy Brooklyn, New York at Sunday dollars etc. if this is not possible this app with get the letter to Moshiach.\n\n\nSample Letter:\n\n26th of Iyar 5776\n\nRebbe King Moshiach Shlita!\nWe are happy to notify your majesty that we have put up 25 Mezuzahs today on three different homes. One of the people Avraham ben Sarah, asked for a Berachah. We hope in this merit we will see the building of the Beis Hamikdosh now! Long live the Rebbe King Moshiach Forever!\nSincerely,\nNasan Ben Serach"
+        textView.text = text
+        textView.editable = false
         
     }
 
